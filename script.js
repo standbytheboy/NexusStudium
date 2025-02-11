862be9673b145a065ec53143ad9dd9d119284088
standby <moraislcs05@gmail.com>
2025-02-10 23:43:12 -0300

    trocando carrossel principal e atualizando design do modal de login

---
+++ script.js
@@ -0,0 +1,33 @@
+// ==================== CARROSSEL ====================
+const panels = document.querySelectorAll('.panel')
+panels.forEach(panel => {
+    panel.addEventListener('click', () => {
+        removeActiveClasses()
+        panel.classList.add('active')
+    })
+})
+
+function removeActiveClasses() {
+    panels.forEach(panel => {
+        panel.classList.remove('active')
+    })
+}
+
+// ==================== MODAL DE LOGIN ====================
+const modal = document.getElementById("myModal");
+const btn = document.getElementById("openModalBtn");
+const span = document.getElementsByClassName("close")[0];
+
+btn.onclick = function () {
+    modal.style.display = "block";
+}
+
+span.onclick = function () {
+    modal.style.display = "none";
+}
+
+window.onclick = function (event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
