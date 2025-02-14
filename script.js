6655c910f967a695644a940d88cd08a51a0cbf8f
Morais <moraislcs05@gmail.com>
2025-02-13 21:16:26 -0300

    finalizando sidebar e iniciando tela de exercicios

--- script.js
+++ script.js
@@ -32,35 +32,6 @@ window.onclick = function (event) {
     }
 }
 
-// ==================== MENU LATERAL / SIDEBAR ====================
-
-const showMenu = document.getElementById("showMenu");
-const sidebar = document.querySelector(".sidebar");
-
-showMenu.addEventListener("click", () => {
-  sidebar.classList.toggle("ocult-sidebar");
+window.addEventListener('load', () => {
+    document.getElementById('loading').style.display = 'none';
 });
-
-const subMenus = document.querySelectorAll(".sub-menu"),
-  buttons = document.querySelectorAll(".sidebar ul button");
-
-const onClick = (item) => {
-  subMenus.forEach((menu) => (menu.style.height = "0px"));
-  buttons.forEach((button) => button.classList.remove("active"));
-
-  if (!item.nextElementSibling) {
-    item.classList.add("active");
-    return;
-  }
-
-  const subMenu = item.nextElementSibling,
-    ul = subMenu.querySelector("ul");
-
-  if (!subMenu.clientHeight) {
-    subMenu.style.height = `${ul.clientHeight}px`;
-    item.classList.add("active");
-  } else {
-    subMenu.style.height = "0px";
-    item.classList.remove("active");
-  }
-};
