64b69bd7ae33b049759d8c12b93fe5925e440327
douglas <doug.balbino@hotmail.com>
2025-02-14 21:38:53 -0300

    ainda adicionandoi as funcoes na pagina help

--- doug/script.js
+++ doug/script.js
@@ -4,4 +4,14 @@ togglesFaq.forEach(toggle => {
     toggle.addEventListener('click', () => {
         toggle.parentNode.classList.toggle('active2')
     })
+});
+
+document.getElementById("help_faq").addEventListener("click", () => {
+    let faqContainer = document.getElementById("faq_container");
+        if (faqContainer.style.display === "none" || faqContainer.style.display === "") {
+            faqContainer.style.display ==="block";
+        } else {
+            faqContainer.style.display = "none";
+        };
+
 })
