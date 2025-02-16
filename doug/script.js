da43ea95b977ddcc0156156abcfce0cf98c825ec
standby <moraislcs05@gmail.com>
2025-02-16 15:53:42 -0300

    ajustando css pagina de ajuda

--- doug/script.js
+++ doug/script.js
@@ -1,8 +1,11 @@
 function togglePainel(painelId) {
-    const painel = document.getElementById(painelId);
-    if (painel.style.display === "none" || painel.style.display === "") {
-        painel.style.display = "block";
-    } else {
-        painel.style.display = "none";
-    }
+    const paineis = document.querySelectorAll('.painel');
+    
+    paineis.forEach(painel => {
+        if (painel.id === painelId) {
+            painel.style.display = painel.style.display === "block" ? "none" : "block";
+        } else {
+            painel.style.display = "none";
+        }
+    });
 }
